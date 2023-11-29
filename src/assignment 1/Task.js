41d27e1298f47039ca9defda27f10673e773b68c
MansoorMusthafa <msmansoor523@gmail.com>
2023-11-29 15:40:37 +0530

    GoWatrAssignment

---
+++ src/assignment 1/Task.js
@@ -0,0 +1,212 @@
+import React, { useRef, useState } from "react";
+import "./TaskStyles.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Task = () => {
+  const [lists, setList] = useState([
+    {
+      id: 1,
+      name: "HTML",
+      price: "1",
+    },
+    {
+      id: 2,
+      name: "CSS",
+      price: "1",
+    },
+  ]);
+  const [updateState, setUpdateState] = useState(-1);
+
+  function handleEdit(id) {
+    setUpdateState(id);
+  }
+  const deleteFun = () =>
+    toast.success("🦄 Skills Deleted Successfully", {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  function handleDelete(id) {
+    const newlist = lists.filter((li) => li.id !== id);
+    setList(newlist);
+    deleteFun();
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    const price = event.target.elements.price.value;
+
+    const newlist = lists.map((li) =>
+      li.id === updateState ? { ...li, name: name, price: price } : li
+    );
+
+    setList(newlist);
+    setUpdateState(-1);
+  }
+
+  return (
+    <div className="crud">
+      <div>
+        <AddList setList={setList} lists={lists} />
+        <form onSubmit={handleSubmit}>
+          <table>
+            <tbody>
+              {lists.map((current) =>
+                updateState === current.id ? (
+                  <EditList
+                    key={current.id}
+                    current={current}
+                    lists={lists}
+                    setList={setList}
+                  />
+                ) : (
+                  <tr key={current.id}>
+                    <td>{current.name}</td>
+                    <td>{current.price}</td>
+                    <td>
+                      <button
+                        className="edit delete"
+                        onClick={() => handleEdit(current.id)}
+                      >
+                        Edit
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="delete"
+                        type="button"
+                        onClick={() => handleDelete(current.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+function EditList({ current, lists, setList }) {
+  function handInputname(event) {
+    const value = event.target.value;
+    const newlist = lists.map((li) =>
+      li.id === current.id ? { ...li, name: value } : li
+    );
+
+    setList(newlist);
+  }
+
+  function handInputprice(event) {
+    const value = event.target.value;
+    const newlist = lists.map((li) =>
+      li.id === current.id ? { ...li, price: value } : li
+    );
+
+    setList(newlist);
+  }
+
+  function handleUpdate(event) {
+    event.preventDefault();
+    // Logic for update if needed
+  }
+
+  return (
+    <tr key={current.id}>
+      <td>
+        <input
+          type="text"
+          onChange={handInputname}
+          name="name"
+          value={current.name}
+        />
+      </td>
+      <td>
+        <input
+          type="text"
+          onChange={handInputprice}
+          name="price"
+          value={current.price}
+        />
+      </td>
+      <td>
+        <button onClick={handleUpdate}>Update</button>
+      </td>
+    </tr>
+  );
+}
+
+function AddList({ setList, lists }) {
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const newId = lists.length + 1; // Generate a new ID based on the length of the existing list
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    const price = event.target.elements.price.value;
+    const newlist = {
+      id: newId,
+      name,
+      price,
+    };
+    setList((prevList) => {
+      return [...prevList, newlist];
+    });
+    nameRef.current.value = "";
+    priceRef.current.value = "";
+  }
+  const notify = () =>
+    toast.success("🦄 Skills Added Successfully", {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  return (
+    <div className="addContainer wrapper">
+      <form className="addForm" onSubmit={handleSubmit}>
+        <h1>Add Skills</h1>
+        <div className="input-box">
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Skills"
+            ref={nameRef}
+          />
+        </div>
+        <div className="input-box">
+          <input
+            type="text"
+            name="price"
+            placeholder="Enter Experience"
+            ref={priceRef}
+          />
+        </div>
+        <div className="end">
+          <button className="btn" type="submit" onClick={notify}>
+            Add
+          </button>
+          <ToastContainer />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Task;
